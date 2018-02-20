736c9a4031b3cd977114c5b581e670e413c1b31a
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-02-20 13:13:30 -0600

    Crashable Abstract Class

---
+++ Space Defender/Assets/Scripts/CrashableObjects/Crashable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Crashable : MonoBehaviour {
+
+    public CrashableType CrashableType;
+    public int Health;
+    public int damageModifier;
+    public float damageMultiplier;
+
+    public int DealDamage() {
+        float damageReturn = 0;
+        switch (CrashableType) {
+            case CrashableType.Standard:
+                damageReturn = Health / 2;
+                break;
+            case CrashableType.Explosive:
+                damageReturn = Health * 2;
+                break;
+            case CrashableType.Penetrating:
+                damageReturn = Health;
+                break;
+            case CrashableType.Ship:
+                damageReturn = Health;
+                break;
+        }
+        damageReturn = ( damageReturn * damageMultiplier ) + damageModifier;
+        return ( int )damageReturn;
+    }
+
+    protected void ReceiveDamage(Crashable crashable) {
+        Health -= crashable.DealDamage();
+        if (Health<=0) {
+            DestroySelf();
+        }
+    }
+
+    protected abstract void DestroySelf();
+}
+
+public enum CrashableType{
+    Ship, Explosive, Penetrating, Standard
+}

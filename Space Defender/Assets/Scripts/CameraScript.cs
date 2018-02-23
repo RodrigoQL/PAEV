e7c0d6f153a57efada31cc5a42dec8289c8b3386
Rodrigo Quiroz de Luna <quiroz.rod@gmail.com>
2018-02-23 13:24:56 -0600

    Camera Size
    Canvas Scaling with screen size
    Screen Aspect Ratio always the same

---
+++ Space Defender/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    private Camera myCam;
+
+    private const float KEEP_ASPECT = 16 / 9f;
+    private void Start() {
+        myCam = GetComponent<Camera>();
+        float aspectRatio = Screen.width / ((float)Screen.height);
+        float percentage = 1 - (aspectRatio / KEEP_ASPECT);
+
+        myCam.rect = new Rect(0f, (percentage / 2), 1f, (1 - percentage));
+    }
+}
